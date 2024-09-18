e9779653fcaa28e9f8289112b0a6befb17c4d465
Bagas Setiaji <stjbagas28@gmail.com>
2024-09-18 09:09:51 +0700

    fix error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-[#600160] to-[#0d000d] text-white">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <h2 className="text-2xl mb-8">Oops! Page not found</h2>
-      <p className="text-lg mb-8">The page you're looking for doesn't exist or has been moved.</p>
+      <p className="text-lg mb-8">The page you&apos;re looking for doesn&apos;t exist or has been moved.</p>
       <Link href="/" className="bg-[#C43CCB] hover:bg-[#a033a7] text-white font-bold py-2 px-4 rounded transition duration-300">
         Go back home
       </Link>
